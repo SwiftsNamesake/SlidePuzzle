6d811ef145b4261d6280ecbd83407b48abcb29d3
Jonatan H Sundqvist <jonatanhsundqvist@gmail.com>
2014-08-06 11:28:37 +0200

    Renamed renderPuzzle to updateTiles, working on createPuzzle

--- main.py
+++ main.py
@@ -8,6 +8,7 @@
 
 import tkinter
 from PIL.ImageTk import PhotoImage, Image
+from random import randint
 
 
 def loadImageTiles(fn):
@@ -26,15 +27,20 @@ def onClick(event):
 
 
 def createPuzzle(fn, rows, columns):
+
 	''' '''
+
 	image = Image.open(fn)
-	width, height = image.size[0]//columns, image.size[1]//rows
+	width, height 	= image.size[0]//columns, image.size[1]//rows 	# Width and height of each tile
+	blankX, blankY 	= randint(rows), randint(columns) 				# Choose an empty tile randomly
+	
 	puzzle = [ [PhotoImage(image.crop((column*width, row*height, (column+1)*width, (row+1)*height))) for row in range(rows)] for column in range(columns)]
+	puzzle[blankX][blankY] = None
 
 	return puzzle
 
 
-def renderPuzzle(puzzle):
+def updateTiles(puzzle):
 	''' '''
 	pass
 
